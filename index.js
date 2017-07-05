29c6d734280d5ff9cf33c924bdc483afa6dcd394
taeshik Hwang <ghjkghj@naver.com>
2017-07-05 16:05:04 +0900

    Write a GraphQL Mutation

--- index.js
+++ index.js
@@ -5,66 +5,94 @@ const graphqlHTTP = require('express-graphql');
 const {
   GraphQLSchema,
   GraphQLObjectType,
+  GraphQLList,
+  GraphQLNonNull,
   GraphQLID,
   GraphQLString,
-  GraphQLNonNull,
-  GraphQLList,
   GraphQLInt,
-  GraphQLBoolean
+  GraphQLBoolean,
 } = require('graphql');
-const { getVideoById, getVideos } = require('./src/data');
+const { getVideoById, getVideos, createVideo } = require('./src/data');
 
 const PORT = process.env.PORT || 3000;
 const server = express();
 
 const videoType = new GraphQLObjectType({
-  name: 'video',
+  name: 'Video',
   description: 'A video on Egghead.io',
   fields: {
     id: {
       type: GraphQLID,
-      description: 'The ID of the video.'
+      description: 'The id of the video.',
     },
     title: {
       type: GraphQLString,
-      description: 'The title of the video.'
+      description: 'The title of the video.',
     },
     duration: {
       type: GraphQLInt,
-      description: 'The duration of the video (in seconds).'
+      description: 'The duration of the video (in seconds).',
     },
-    watched: {
+    released: {
       type: GraphQLBoolean,
-      description: 'Whether or not the viewer has watched the video.'
-    }
-  }
+      description: 'Whether or not the video has been released.',
+    },
+  },
 });
 
 const queryType = new GraphQLObjectType({
   name: 'QueryType',
-  description: 'The root query type',
+  description: 'The root query type.',
   fields: {
     videos: {
       type: new GraphQLList(videoType),
-      resolve: getVideos
+      resolve: getVideos,
     },
     video: {
       type: videoType,
       args: {
         id: {
           type: new GraphQLNonNull(GraphQLID),
-          description: 'The ID of the video.'
-        }
+          description: 'The id of the video.',
+        },
       },
       resolve: (_, args) => {
         return getVideoById(args.id);
-      }
-    }
-  }
+      },
+    },
+  },
+});
+
+const mutationType = new GraphQLObjectType({
+  name: 'Mutation',
+  description: 'The root Mutation type.',
+  fields: {
+    createVideo: {
+      type: videoType,
+      args: {
+        title: {
+          type: new GraphQLNonNull(GraphQLString),
+          description: 'The title of the video.',
+        },
+        duration: {
+          type: new GraphQLNonNull(GraphQLInt),
+          description: 'The duration of the video (in seconds).',
+        },
+        released: {
+          type: new GraphQLNonNull(GraphQLBoolean),
+          description: 'Whether or not the video is released.',
+        },
+      },
+      resolve: (_, args) => {
+        return createVideo(args);
+      },
+    },
+  },
 });
 
 const schema = new GraphQLSchema({
-  query: queryType
+  query: queryType,
+  mutation: mutationType,
 });
 
 server.use('/graphql', graphqlHTTP({
@@ -73,5 +101,5 @@ server.use('/graphql', graphqlHTTP({
 }));
 
 server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on http://localhost:${PORT}`);
 });
